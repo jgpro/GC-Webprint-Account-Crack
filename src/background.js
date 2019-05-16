ccffefb93074793ea2fc1426b31692731f72388f
jgpro <kingjgpro@gmail.com>
2019-05-17 10:16:04 +1200

    Update background.js
    Fixed storage so JSON data can be pushed to the initial empty object.

--- src/background.js
+++ src/background.js
@@ -8,6 +8,9 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('The color is green.');
   });
+  chrome.storage.sync.set({savedKeys: '{}'}, () => {
+    console.log('Storage Setup Conplete');
+  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
